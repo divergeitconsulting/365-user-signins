f2a337dc145ef52d1dbb272215f55a931ab93f03
Connor Bescos <connorb@divergeit.com>
2022-08-17 11:21:30 -0700

    Main

---
+++ GetUserGroups.ps1
@@ -0,0 +1,17 @@
+Connect-azuread
+
+$guests = Get-AzureADUser | Where-Object {($_.UserType -eq "Guest")}
+foreach($guest in $guests){
+    $groups= Get-AzureADUserMembership -ObjectId $guest.ObjectId
+    foreach($group in $groups){
+        $exportvalues = New-Object psobject
+        $exportvalues | add-member -MemberType NoteProperty -Name UserPrincipalName -Value $guest.UserPrincipalName
+        $exportvalues | add-member -MemberType NoteProperty -Name UserEmail -Value $guest.mail
+        $exportvalues | add-member -MemberType NoteProperty -Name UserObjectID -Value $guest.ObjectId
+        $exportvalues | add-member -MemberType NoteProperty -Name UserDisplayName -Value $group.DisplayName
+        $exportvalues | add-member -MemberType NoteProperty -Name GroupDisplayName -Value $group.DisplayName
+        $exportvalues | add-member -MemberType NoteProperty -Name GroupObjectId -Value $group.ObjectId
+        $exportvalues | Export-CSV -path $env:userprofile\guestgroups.csv -Append -NoTypeInformation
+    }
+}
+
