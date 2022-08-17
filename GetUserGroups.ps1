08b3f4baff24bdb6992b862527c0a7fe02d05595
Connor Bescos <connorb@divergeit.com>
2022-08-17 11:26:25 -0700

    Main

--- GetUserGroups.ps1
+++ GetUserGroups.ps1
@@ -8,7 +8,7 @@ foreach($guest in $guests){
         $exportvalues | add-member -MemberType NoteProperty -Name UserPrincipalName -Value $guest.UserPrincipalName
         $exportvalues | add-member -MemberType NoteProperty -Name UserEmail -Value $guest.mail
         $exportvalues | add-member -MemberType NoteProperty -Name UserObjectID -Value $guest.ObjectId
-        $exportvalues | add-member -MemberType NoteProperty -Name UserDisplayName -Value $group.DisplayName
+        $exportvalues | add-member -MemberType NoteProperty -Name UserDisplayName -Value $guest.DisplayName
         $exportvalues | add-member -MemberType NoteProperty -Name GroupDisplayName -Value $group.DisplayName
         $exportvalues | add-member -MemberType NoteProperty -Name GroupObjectId -Value $group.ObjectId
         $exportvalues | Export-CSV -path $env:userprofile\guestgroups.csv -Append -NoTypeInformation
